2074d9f42debd3ed9d5c8d33dce17c0a8547afc6
cheralathan-3 <cheralathan.3.3@protosem.tech>
2025-04-01 10:41:11 +0530

    minor fix - bact to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,23 +48,23 @@ typeText();
 // });
 
 
-// // When the user scrolls down from the top of the document, show the button
-// window.onscroll = function () {
-//     scrollFunction();
-// };
+// When the user scrolls down from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
 
-// function scrollFunction() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         backToTopButton.style.display = "block";
-//     } else {
-//         backToTopButton.style.display = "none";
-//     }
-// }
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
 
-// // When the user clicks on the button, scroll to the top of the document
-// backToTopButton.addEventListener("click", function () {
-//     // For Safari
-//     document.body.scrollTop = 0;
-//     // For Chrome, Firefox, IE and Opera
-//     document.documentElement.scrollTop = 0;
-// });
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.addEventListener("click", function () {
+    // For Safari
+    document.body.scrollTop = 0;
+    // For Chrome, Firefox, IE and Opera
+    document.documentElement.scrollTop = 0;
+});
